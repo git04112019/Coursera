c552e093e7d0385229e1cd8c578048127e0c018e
Ziqiang Guan <ziqiang.guan@yahoo.com>
2016-11-25 21:17:37 -0500

    fixed indentation

--- Algorithm/union_find.rb
+++ Algorithm/union_find.rb
@@ -1,97 +1,97 @@
 class UnionFind
-	attr_reader :unionSets
-	attr_reader :connections
-	attr_accessor :debug
+    attr_reader :unionSets
+    attr_reader :connections
+    attr_accessor :debug
 
-	def initialize(connections)
-		@unionSets = []
-		@connections = []
-		@debug = false
+    def initialize(connections)
+        @unionSets = []
+        @connections = []
+        @debug = false
 
-		connections.each do |connection|
-			union(connection[0], connection[1])
-			@connections.push(connection)
-		end
-	end
+        connections.each do |connection|
+            union(connection[0], connection[1])
+            @connections.push(connection)
+        end
+    end
 
-	def union(p, q, findNumInSet = false)
-		# if currently connected
-		if(@connections.include?([p, q]) || @connections.include?([q, p]))
-			return
-		end
+    def union(p, q, findNumInSet = false)
+        # if currently connected
+        if(@connections.include?([p, q]) || @connections.include?([q, p]))
+            return
+        end
 
-		if(findNumInSet)
-			sets = findNumInSet([p, q])
-		else
-			sets = findSetForNum([p, q])
-		end
+        if(findNumInSet)
+            sets = findNumInSet([p, q])
+        else
+            sets = findSetForNum([p, q])
+        end
 
-		if (sets[p] == nil && sets[q] == nil)
-			puts "neither #{p} nor #{q} are in a set" if @debug
-			@unionSets.push([p, q])
-			@connections.push([p, q])
-		elsif (sets[p] != nil && sets[q] == nil)
-			puts "#{q} is not in a set" if @debug
-			sets[p].push(q)
-			@connections.push([p, q])
-		elsif (sets[p] == nil && sets[q] != nil)
-			puts "#{p} is not in a set" if @debug
-			sets[q].push(p)
-			@connections.push([p, q])
-		else # both of them are in a set
-			unless (sets[p] == sets[q])
-				puts "both #{p} and #{q} are in a set" if @debug
-				@unionSets.delete(sets[p])
-				@unionSets.delete(sets[q])
-				@unionSets.push(sets[p] | sets[q])
-			else
-				puts "#{p} and #{q} are in the same set already" if @debug
-			end
-		end
-	end
+        if (sets[p] == nil && sets[q] == nil)
+            puts "neither #{p} nor #{q} are in a set" if @debug
+            @unionSets.push([p, q])
+            @connections.push([p, q])
+        elsif (sets[p] != nil && sets[q] == nil)
+            puts "#{q} is not in a set" if @debug
+            sets[p].push(q)
+            @connections.push([p, q])
+        elsif (sets[p] == nil && sets[q] != nil)
+            puts "#{p} is not in a set" if @debug
+            sets[q].push(p)
+            @connections.push([p, q])
+        else # both of them are in a set
+            unless (sets[p] == sets[q])
+                puts "both #{p} and #{q} are in a set" if @debug
+                @unionSets.delete(sets[p])
+                @unionSets.delete(sets[q])
+                @unionSets.push(sets[p] | sets[q])
+            else
+                puts "#{p} and #{q} are in the same set already" if @debug
+            end
+        end
+    end
 
-	def connected(p, q)
-	end
+    def connected(p, q)
+    end
 
-	def find(p)
+    def find(p)
 
-	end
+    end
 
-	def count()
+    def count()
 
-	end
+    end
 
-	private
-	def findNumInSet(arrayOfNums)
-		numToSet = Hash.new
-		
-		arrayOfNums.each do |num|
-			@unionSets.each do |set|
-				if(set.include?(num))
-					numToSet[num] = set
-					break
-				end
-			end
-		end
+    private
+    def findNumInSet(arrayOfNums)
+        numToSet = Hash.new
+        
+        arrayOfNums.each do |num|
+            @unionSets.each do |set|
+                if(set.include?(num))
+                    numToSet[num] = set
+                    break
+                end
+            end
+        end
 
-		return numToSet
-	end
+        return numToSet
+    end
 
-	def findSetForNum(arrayOfNums)
-		arrayOfNums.uniq!
-		numToSet = Hash.new
+    def findSetForNum(arrayOfNums)
+        arrayOfNums.uniq!
+        numToSet = Hash.new
 
-		@unionSets.each do |set|
-			arrayOfNums.each do |num|
-				if(set.include?(num))
-					if(numToSet[num] != nil) # if true, number is appearing in multiple sets
-						puts "UnionFind.findSetForNum found same number in multiple unionSets" if @debug
-						return
-					end
-					numToSet[num] = set
-				end
-			end
-		end
-		return numToSet
-	end
+        @unionSets.each do |set|
+            arrayOfNums.each do |num|
+                if(set.include?(num))
+                    if(numToSet[num] != nil) # if true, number is appearing in multiple sets
+                        puts "UnionFind.findSetForNum found same number in multiple unionSets" if @debug
+                        return
+                    end
+                    numToSet[num] = set
+                end
+            end
+        end
+        return numToSet
+    end
 end
--- Algorithm/union_find.tests.rb
+++ Algorithm/union_find.tests.rb
@@ -3,40 +3,40 @@ require "test/unit"
 require "benchmark"
 
 class UFTest < Test::Unit::TestCase
-    def setup
-        @testData = []
+	def setup
+		@testData = []
 
-        input = [[4, 3],[3, 8],[6, 5],[9, 4],[2, 1],[8, 9],[5, 0],[7, 2],[6, 1],[1, 0],[6, 7]]
-        output = [[4, 3, 8, 9], [6, 5, 0, 2, 1, 7]]
+		input = [[4, 3],[3, 8],[6, 5],[9, 4],[2, 1],[8, 9],[5, 0],[7, 2],[6, 1],[1, 0],[6, 7]]
+		output = [[4, 3, 8, 9], [6, 5, 0, 2, 1, 7]]
+		
+		@testData.push [input, output]
 
-        @testData.push [input, output]
+		@uf = UnionFind.new(@testData[0][0])
+		@uf.debug = true
+	end
 
-        @uf = UnionFind.new(@testData[0][0])
-        @uf.debug = true
-    end
+	def teardown
+	end
 
-    def teardown
-    end
+	def testHasCorrectDefaultSet
+		assert(@uf.unionSets == @testData[0][1])
+	end
 
-    def testHasCorrectDefaultSet
-        assert(@uf.unionSets == @testData[0][1])
-    end
+	def testFindNumInSet
+		puts
+		puts "findNumInSet benchmark"
+		Benchmark.bm do |x|
+			x.report { @uf.union(9, 7) }
+		end
+		assert(@uf.unionSets == [[4, 3, 8, 9, 6, 5, 0, 2, 1, 7]])
+	end
 
-    def testFindNumInSet
-        puts
-        puts "findNumInSet benchmark"
-        Benchmark.bm do |x|
-            x.report { @uf.union(9, 7) }
-        end
-        assert(@uf.unionSets == [[4, 3, 8, 9, 6, 5, 0, 2, 1, 7]])
-    end
-
-    def testFindSetForNum
-        puts
-        puts "findSetInNum benchmark"
-        Benchmark.bm do |x|
-            x.report { @uf.union(9, 7, true)}
-        end
-        assert(@uf.unionSets == [[4, 3, 8, 9, 6, 5, 0, 2, 1, 7]])
-    end
+	def testFindSetForNum
+		puts
+		puts "findSetInNum benchmark"
+		Benchmark.bm do |x|
+			x.report { @uf.union(9, 7, true)}
+		end
+		assert(@uf.unionSets == [[4, 3, 8, 9, 6, 5, 0, 2, 1, 7]])
+	end
 end
