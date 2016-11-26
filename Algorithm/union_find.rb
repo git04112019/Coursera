51e8537be9a9deab2f28d6c4336f69458d936314
Ziqiang Guan <ziqiang.guan@yahoo.com>
2016-11-25 20:59:27 -0500

    finalized union

--- Algorithm/union_find.rb
+++ Algorithm/union_find.rb
@@ -1,8 +1,10 @@
 class UnionFind
-	attr_reader :union
+	attr_reader :unionSets
+	attr_reader :connections
+	attr_accessor :debug
 
-	def initialize(connections = Test.connections)
-		@union = []
+	def initialize(connections)
+		@unionSets = []
 		@connections = []
 		@debug = false
 
@@ -12,18 +14,21 @@ class UnionFind
 		end
 	end
 
-	def union(p, q)
+	def union(p, q, findNumInSet = false)
 		# if currently connected
 		if(@connections.include?([p, q]) || @connections.include?([q, p]))
 			return
 		end
 
-		sets = findNumInSet([p, q])
-		# sets = findSetForNum([p, q])
+		if(findNumInSet)
+			sets = findNumInSet([p, q])
+		else
+			sets = findSetForNum([p, q])
+		end
 
 		if (sets[p] == nil && sets[q] == nil)
 			puts "neither #{p} nor #{q} are in a set" if @debug
-			@union.push([p, q])
+			@unionSets.push([p, q])
 			@connections.push([p, q])
 		elsif (sets[p] != nil && sets[q] == nil)
 			puts "#{q} is not in a set" if @debug
@@ -36,9 +41,9 @@ class UnionFind
 		else # both of them are in a set
 			unless (sets[p] == sets[q])
 				puts "both #{p} and #{q} are in a set" if @debug
-				@union.delete(sets[p])
-				@union.delete(sets[q])
-				@union.push(sets[p] | sets[q])
+				@unionSets.delete(sets[p])
+				@unionSets.delete(sets[q])
+				@unionSets.push(sets[p] | sets[q])
 			else
 				puts "#{p} and #{q} are in the same set already" if @debug
 			end
@@ -57,26 +62,36 @@ class UnionFind
 	end
 
 	private
-		# for each num in arrayOfNums, search sets
-		def findNumInSet(arrayOfNums)
-			numToSet = Hash.new
-			
-			arrayOfNums.each do |num|
-				@union.each do |set|
-					if(set.include?(num))
-						numToSet[num] = set
-						break
-					end
+	def findNumInSet(arrayOfNums)
+		numToSet = Hash.new
+		
+		arrayOfNums.each do |num|
+			@unionSets.each do |set|
+				if(set.include?(num))
+					numToSet[num] = set
+					break
 				end
 			end
-
-			return numToSet
 		end
 
-		# for each set, search the num in arrayOfNums
-		def findSetForNum(arrayOfNums)
-			numToSet = Hash.new
+		return numToSet
+	end
 
-			return numToset
+	def findSetForNum(arrayOfNums)
+		arrayOfNums.uniq!
+		numToSet = Hash.new
+
+		@unionSets.each do |set|
+			arrayOfNums.each do |num|
+				if(set.include?(num))
+					if(numToSet[num] != nil) # if true, number is appearing in multiple sets
+						puts "UnionFind.findSetForNum found same number in multiple unionSets" if @debug
+						return
+					end
+					numToSet[num] = set
+				end
+			end
 		end
+		return numToSet
+	end
 end
